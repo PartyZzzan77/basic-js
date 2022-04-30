f4cdd88b68b4f25744ae7519af4387a8ad591124
Mitya <partyzan327@gmail.com>
2022-04-30 09:23:10 +0300

    feat: implement a function carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,15 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-}
+function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity >= 15 || sampleActivity < 1) {
+    return false;
+  }
 
-// const MODERN_ACTIVITY = Math.log10(sampleActivity).toFixed(3);
-// const HALF_LIFE_PERIOD = Math.log(2).toFixed(3);
-// console.log(MODERN_ACTIVITY);
-// console.log(HALF_LIFE_PERIOD);
-// return HALF_LIFE_PERIOD * 100 / MODERN_ACTIVITY;
+  return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / (Math.log(2) / HALF_LIFE_PERIOD))
+}
 module.exports = {
   dateSample
 };
