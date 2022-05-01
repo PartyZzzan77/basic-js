2a6aa345299a639c1c91e84f88dcae11cae3eb89
Mitya <partyzan327@gmail.com>
2022-05-01 18:16:02 +0300

    feat: implement a function simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  buffer: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.buffer.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.buffer.push(String(value));
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (isNaN(position) || parseInt(position) > this.buffer.length || position < 1) {
+      this.buffer = [];
+      throw new Error('You can\'t remove incorrect link!')
+    }
+    let index = position - 1;
+    this.buffer.splice(index, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.buffer = this.buffer.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const target = this.buffer
+      .map(elem => `( ${(elem)} )`)
+      .join('~~');
+
+    this.buffer = [];
+    return target;
   }
 };
 
