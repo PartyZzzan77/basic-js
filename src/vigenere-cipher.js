e7cc4eaa5f4bd09e6cc6280738442a400064bda3
Mitya <partyzan327@gmail.com>
2022-05-01 15:45:57 +0300

    feat: implement a function vigener-chiper.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  #isUpperCase(letter) {
+    let l = letter.charCodeAt();
+    if (l >= 65 && l <= 90) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  #isLowerCase(letter) {
+    let l = letter.charCodeAt();
+    if (l >= 97 && l <= 122) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    let cypherBuffer = [];
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentLetter = message[i];
+      if (this.#isUpperCase(currentLetter)) {
+        let upperLetter = ((currentLetter.charCodeAt() - 65) + (key[j % key.length].toUpperCase().charCodeAt() - 65)) % 26;
+        cypherBuffer.push(String.fromCharCode(upperLetter + 65).toUpperCase());
+        j++;
+      } else if (this.#isLowerCase(currentLetter)) {
+        let lowerLetter = ((currentLetter.charCodeAt() - 97) + (key[j % key.length].toLowerCase().charCodeAt() - 97)) % 26;
+        cypherBuffer.push(String.fromCharCode(lowerLetter + 97).toUpperCase())
+        j++;
+      } else {
+        cypherBuffer.push(currentLetter.toUpperCase());
+      }
+    }
+    return this.direction ? cypherBuffer.join('') : cypherBuffer.reverse().join('');
+  };
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    let cypherBuffer = [];
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentLetter = message[i];
+      if (this.#isUpperCase(currentLetter)) {
+        let upperLetter = ((currentLetter.charCodeAt() - 65) - (key[j % key.length].toUpperCase().charCodeAt() - 65) + 26) % 26;
+        cypherBuffer.push(String.fromCharCode(upperLetter + 65).toUpperCase());
+        j++;
+      } else if (this.#isLowerCase(currentLetter)) {
+        let lowerLetter = ((currentLetter.charCodeAt() - 97) - (key[j % key.length].toLowerCase().charCodeAt() - 97) + 26) % 26;
+        cypherBuffer.push(String.fromCharCode(lowerLetter + 97).toUpperCase())
+        j++;
+      } else {
+        cypherBuffer.push(currentLetter.toUpperCase());
+      }
+    }
+    return this.direction ? cypherBuffer.join('') : cypherBuffer.reverse().join('');
+  };
 }
 
 module.exports = {
